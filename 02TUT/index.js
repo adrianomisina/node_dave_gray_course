3f210ceecb4937a7d8f76d37b07b5715c76bddae
Adriano Misina <adriano.misina@agroboard.com.br>
2023-10-15 06:05:15 -0300

    read and write file

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -1,22 +1,42 @@
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8',(err, data) => {
-    if (err) throw err;
-    console.log(data);
-})
+const fileOps = async () => {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
+        console.log(data);
 
-console.log('Hello...');
+        await fsPromises.unlink(path.join(__dirname, 'files','starter.txt'));
 
-fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you',(err) => {
-    if (err) throw err;
-    console.log(`Write complete`);
-})
+        await fsPromises.writeFile(path.join(__dirname, 'files','promiseWrite.txt'), data);
+        await fsPromises.appendFile(path.join(__dirname, 'files','promiseWrite.txt'), `\n\nNice to meet you.`);
+        await fsPromises.rename(path.join(__dirname, 'files','promiseWrite.txt'),path.join(__dirname, 'files','promiseComplete.txt'));
+        
+        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
+        console.log(newData)
 
-fs.appendFileFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you',(err) => {
-    if (err) throw err;
-    console.log(`Write complete`);
-})
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+fileOps();
+
+// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.',(err) => {
+//     if (err) throw err;
+//     console.log(`Write complete`);
+
+//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), `\n\nYes it is.`,(err) => {
+//         if (err) throw err;
+//         console.log(`Append complete`);
+
+//         fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'),(err) => {
+//             if (err) throw err;
+//             console.log(`Rename complete`);
+//         })
+//     })
+// })
+
 
 //exit on uncaught error
 process.on('uncaughtException', err => {
