217715a03792faa0c57ec38cf1433b5ae70ec0ca
Adriano Misina <adriano.misina@agroboard.com.br>
2023-11-02 06:43:13 -0300

    middleware

--- 06TUT/server.js
+++ 06TUT/server.js
@@ -1,36 +1,41 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
-const logEvents = require('./middleware/logEvents')
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
-//custom middleware logger
-app.use((req, res, next) => {
-    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt')
-    console.log(`${req.method} ${req.path}`);
-    next();
-})
+// custom middleware logger
+app.use(logger);
 
-//built-in middleware to handle urlencoded data
-//in oder words, form data:
-// 'content-type: application/x-www-form-urlencoded'
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
-//middleware integrado para lidar com dados codificados em URL
-//em outras palavras, dados do formulário:
-// 'tipo de conteúdo: aplicativo/x-www-form-urlencoded'
+// built-in middleware to handle urlencoded data
+// in other words, form data:  
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({extended: false}));
-
-//built-in middleware for json;
-//middleware integrado para json;
+// built-in middleware for json 
 app.use(express.json());
 
 //serve static files
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|index(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html', {root: __dirname});
+app.get('^/$|/index(.html)?', (req, res) => {
+    //res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
@@ -39,18 +44,19 @@ app.get('/new-page(.html)?', (req, res) => {
 });
 
 app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 y default
+    res.redirect(301, '/new-page.html'); //302 by default
 });
 
-//Route handlers
+// Route handlers
 app.get('/hello(.html)?', (req, res, next) => {
     console.log('attempted to load hello.html');
-    next();
+    next()
 }, (req, res) => {
-    res.send('Hello, World!')
+    res.send('Hello World!');
 });
 
-//chaining route handlers
+
+// chaining route handlers
 const one = (req, res, next) => {
     console.log('one');
     next();
@@ -63,18 +69,22 @@ const two = (req, res, next) => {
 
 const three = (req, res) => {
     console.log('three');
-    res.send('Finished!')
+    res.send('Finished!');
 }
 
 app.get('/chain(.html)?', [one, two, three]);
 
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-// 2:35:34
